80b69502c5b69c89f051286414fb78f172931789
Rus Daniel <bidstopper@gmail.com>
2017-04-23 11:13:17 +0200

    first commit

---
+++ src/pages/gallery/gallery.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-gallery',
+  templateUrl: 'gallery.html'
+})
+export class GalleryPage {
+
+  constructor(public navCtrl: NavController) {}
+
+}
---
+++ src/pages/personal-information/personal-information.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-personal-information',
+  templateUrl: 'personal-information.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController) {}
+
+}
